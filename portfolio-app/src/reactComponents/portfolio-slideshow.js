35429204a28a034ec9a7ee725a78f111c6a39e2f
Pedro Krieckaert <58333196+PEEKrieckaert@users.noreply.github.com>
2020-01-29 13:46:14 +0100

    Backup 29/01/2020
    Small back up while most of the site is still stable. Will be moving to React states in order to complete the slideshow.

--- portfolio-app/src/reactComponents/portfolio-slideshow.js
+++ portfolio-app/src/reactComponents/portfolio-slideshow.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-fetch('../data/slideShow.json')
+/*fetch('../data/slideShow.json')
   .then(response => {
     return response.json()
   })
@@ -11,22 +11,54 @@ fetch('../data/slideShow.json')
   .catch(err => {
     console.log(err);
   })
+*/
 
-function port_slide(){
-    return (
+var data = {
+    "projects":[
+        {"name": "Project 1",
+        "image": {
+            "src": require("../imgs/testImg.jpg"),
+            "alt": 'Test Image'
+            },
+        "desc": "Lorem Ipsum",
+        "skills":["test 1", "test 2", "test 3"]},
+        {"name": "Project 2",
+        "image": { 
+            "src": require("../imgs/testImg.jpg"),
+            "alt": 'Test Image'
+            },
+        "desc": "Lorem Ipsum",
+        "skills":["test 1", "test 2", "test 3"]}
+    ]
+}
+
+class Slideshow extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = 0
+    }
+
+    changeSlide = () => {
+        this.setState(1);
+    }
+
+    render() {
+        return(
             <div>
                 <div className="content-grid">
                     <div className = "content-head">
                         <h3>Portfolio</h3>
                     </div>
                     <div className = "content-slide">
-                        <img className = "slide-img" id = "proj1-img" alt = "testing" src = ""></img>
-                        <h3 className = "slide-head">Project 1</h3>
-                        <p className = "slide-desc" id = "proj1-desc">Dori me Interimo, adapare Dori me Ameno Ameno Latire Latiremo Dori me Interimo, adapare Dori me Ameno Ameno Latire Latiremo Dori me Interimo, adapare Dori me Ameno Ameno Latire Latiremo</p>
+                        <div className = "slide-img">
+                            <div style= {{"background": `url(${data.projects[this.state].image.src})`}}></div>
+                        </div>
+                        <h3 className = "slide-head">{data.projects[this.state].name}</h3>
+                        <p className = "slide-desc">{data.projects[this.state].desc}</p>
                         <ul className = "slide-skill">
-                            <li>&bull; Hi, there</li>
-                            <li>&bull; Oh, hello</li>
-                            <li>&bull; How's it going?</li>
+                            <li>&bull; {data.projects[this.state].skills[0]}</li>
+                            <li>&bull; {data.projects[this.state].skills[1]}</li>
+                            <li>&bull; {data.projects[this.state].skills[2]}</li>
                         </ul>
                     </div>
                     <div className = "content-nav">
@@ -35,11 +67,14 @@ function port_slide(){
                         <span>&bull;</span>
                         <span>&bull;</span>
                         <span>&#9654;</span>
+                        <button type="button" onClick={this.changeSlide}>Change slide</button>
                     </div>
                 </div>
             </div>
 
-    );
+        );
+        
+    }
 }
 
-export default port_slide;
+export default Slideshow;
